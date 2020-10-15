465e43757cf8917370146a249a2e8867a6275c18
Alexis Tua <alexis.tua@icloud.com>
2020-10-15 00:46:27 -0500

    Agregamos al conversor, la moneda argentina, chilena y mexicana usando condicionales

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -1,7 +1,46 @@
-pesos = input('Cuantos pesos colombianos tienes?:')
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print('Tienes $' + dolares + ' dolares')
+menu = '''
+Bienvenidos al conversor de monedas 💰
+
+1 - Pesos Argentinos
+2 - Pesos Chilenos
+3 - Pesos Colombianos
+4 - Pesos Mexicanos
+
+Elige una opcion: '''
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input('Cuantos pesos Argentinos tienes?:')
+    pesos = float(pesos)
+    valor_dolar = 77.41
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' dolares')
+elif opcion == 2:
+    pesos = input('Cuantos pesos Chilenos tienes?:')
+    pesos = float(pesos)
+    valor_dolar = 798.80
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' dolares')
+elif opcion == 3:
+    pesos = input('Cuantos pesos colombianos tienes?:')
+    pesos = float(pesos)
+    valor_dolar = 3839
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' dolares')
+elif opcion == 4:
+    pesos = input('Cuantos pesos Mexicanos tienes?:')
+    pesos = float(pesos)
+    valor_dolar = 21.36
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' dolares')
+else:
+    print('Por favor ingresa una opcion correcta: ')
